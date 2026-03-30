8dfddaeab2ebf2c7567d08c14563b89127953436
Alex Tat <tatalexandru91@gmail.com>
2026-03-30 09:48:57 +0300

    main: update entry point to initialize database

--- src/main/java/com/searchengine/Main.java
+++ src/main/java/com/searchengine/Main.java
@@ -1,11 +1,16 @@
 package com.searchengine;
 
 import com.searchengine.config.Config;
+import com.searchengine.repository.DatabaseManager;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Config config = Config.load("config.json");
         config.validate();
-        System.out.println(config);
+
+        DatabaseManager db = new DatabaseManager(config.getDatabasePath());
+        db.initialize();
+
+        db.close();
     }
 }
