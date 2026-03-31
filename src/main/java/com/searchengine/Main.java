95e8f101b0fd420f7a0a8a2202bd5d82abe279c4
Alex Tat <tatalexandru91@gmail.com>
2026-03-31 19:01:51 +0300

    main: wire all components together and start CLI

--- src/main/java/com/searchengine/Main.java
+++ src/main/java/com/searchengine/Main.java
@@ -1,14 +1,15 @@
 package com.searchengine;
 
+import com.searchengine.cli.CLI;
 import com.searchengine.config.Config;
-import com.searchengine.model.IndexReport;
+import com.searchengine.indexing.*;
 import com.searchengine.repository.DatabaseManager;
 import com.searchengine.repository.FileRepository;
-import com.searchengine.indexing.*;
-
+import com.searchengine.search.SearchService;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         Config config = Config.load("config.json");
         config.validate();
@@ -17,19 +18,18 @@ public class Main {
         db.initialize();
 
         FileRepository repository = new FileRepository(db.getConnection());
-        FileFilter filter = new FileFilter(config);
-        FileCrawler crawler = new FileCrawler(filter);
-        ChangeDetector changeDetector = new ChangeDetector(repository);
+
+        FileFilter filter           = new FileFilter(config);
+        FileCrawler crawler         = new FileCrawler(filter);
+        ChangeDetector detector     = new ChangeDetector(repository);
         PlainTextExtractor extractor = new PlainTextExtractor(config.getTextExtensions());
 
-        IndexingService indexingService = new IndexingService(
-                config, crawler, filter,
-                List.of(extractor),
-                changeDetector, repository
-        );
+        IndexingService indexingService = new IndexingService(config, crawler, filter, List.of(extractor),
+                detector, repository);
 
-        IndexReport report = indexingService.index();
-        System.out.println(report.generateReport());
+        SearchService searchService = new SearchService(repository);
+        CLI cli = new CLI(indexingService, searchService);
+        cli.start();
 
         db.close();
     }
