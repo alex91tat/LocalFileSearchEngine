6a07a15dffc143af24168dcd75715ddd44ac3fbb
Alex Tat <tatalexandru91@gmail.com>
2026-04-27 20:28:59 +0300

    main: wire AliasManager into SearchService and CLI

--- src/main/java/com/searchengine/Main.java
+++ src/main/java/com/searchengine/Main.java
@@ -5,6 +5,7 @@ import com.searchengine.config.Config;
 import com.searchengine.indexing.*;
 import com.searchengine.repository.DatabaseManager;
 import com.searchengine.repository.FileRepository;
+import com.searchengine.search.AliasManager;
 import com.searchengine.search.RelevanceRanking;
 import com.searchengine.search.SearchHistoryTracker;
 import com.searchengine.search.SearchService;
@@ -29,13 +30,15 @@ public class Main {
         IndexingService indexingService = new IndexingService(config, crawler, filter, List.of(extractor),
                 detector, repository);
 
-        SearchService searchService = new SearchService(repository);
+        AliasManager aliasManager = new AliasManager();
+
+        SearchService searchService = new SearchService(repository, aliasManager);
 
         SearchHistoryTracker historyTracker = new SearchHistoryTracker();
         searchService.addObserver(historyTracker);
         searchService.setRankingStrategy(new RelevanceRanking(historyTracker));
 
-        CLI cli = new CLI(indexingService, searchService, historyTracker);
+        CLI cli = new CLI(indexingService, searchService, historyTracker, aliasManager);
         cli.start();
 
         db.close();
